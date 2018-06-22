f5e01fba00d413785d7c7600fe97178065bde48a
tshyeon <qvil1127@gmail.com>
2018-06-22 17:18:02 +0900

    Add theme props: any

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -3,8 +3,8 @@ import { css } from "styled-components";
 export const CardStyle = css`
   box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
   background-color: white;
-  border-radius: ${props => props.theme.spacing.unit}rem;
-  padding ${props => props.theme.spacing.gutter}rem;
+  border-radius: ${(props: any) => props.theme.spacing.unit}rem;
+  padding ${(props: any) => props.theme.spacing.gutter}rem;
 `;
 
 export { default as theme } from "./theme";
